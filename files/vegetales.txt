f4a64bb2b3c7dda5a04cfe22281fab4da2c01a61
albertofcasuso <albertofcasuso@gmail.com>
2019-10-21 19:11:16 -0500

    More exercises

---
+++ files/vegetales.txt
@@ -0,0 +1,3 @@
+Tomate
+Lechuga
+Cebolla
--- procesado_archivos.py
+++ procesado_archivos.py
@@ -1,23 +1,51 @@
+import os
+import time
+
 #
 #
 #
 # Abrir archivos open(lo que sea) especificando la ruta del archivo
 # Leer archivos .read()
 # Cerrar archivo despues de procesarlo .close()
-
+"""
 archivo = open('frutas.txt')
 contenido = archivo.read()
 archivo.close()
-print(contenido)
-print()
-print()
+#print(contenido)
+#print()
+#print()
 
 #
 #
 #
-# Se puede hacer con with
-
-with open('frutas.txt') as archivo:
+# Se puede hacer con with --- as
+with open('frutas.txt', "r") as archivo:
     content = archivo.read()
 
-print(content)
+
+#
+#
+# Para escribir \n es un salto de línea
+with open('files/vegetales.txt', "w") as archivo:
+    archivo.write("Tomate\nLechuga\nCebolla")
+"""
+with open('files/data.txt', 'a+') as file:
+    file.seek(0)
+    contenido = file.read()
+    file.seek(0)
+    file.write(contenido)
+    file.write(contenido)
+
+
+#
+#
+#
+# Ver si existe un archivo
+# os.path.exists() regresa True o false
+
+while True:
+    if os.path.exists('files/datan.txt'):
+        print('Si existe')
+    else:
+        print('No existe')
+    time.sleep(5)

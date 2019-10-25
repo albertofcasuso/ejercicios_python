0b99f892586a14cbcb6b3949d8196a5a5a419c44
albertofcasuso <albertofcasuso@gmail.com>
2019-10-25 09:06:31 -0500

    Reducido a tres líneas. Optimizado.

--- frase_al_reves.py
+++ frase_al_reves.py
@@ -1,8 +1,3 @@
-def al_reves(frase):
-    palabras = frase.split()
-    reves_al = " ".join(palabras[::-1])
-    return print(str(reves_al))
-
-
-frase = input("Escribe la foking frase: ")
-al_reves(frase)
+while True:
+    frase = input("Escribe la foking frase: ")
+    print(frase[::-1])
